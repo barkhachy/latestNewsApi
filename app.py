5e711bd9575b845d258998608b509f7aa7af39db
barkhachy <barkha11199@gmail.com>
2020-08-01 16:36:39 +0530

    api

--- app.py
+++ app.py
@@ -12,25 +12,25 @@ app = Flask(__name__)
 @app.route('/news',methods = ['GET','POST'])
 def home():
     if request.method=='GET':
-		url = "https://time.com/"
-		res = requests.get(url = url)
-		tree = html.fromstring(res.content )
-		#print(tree)
-		li = []
-		for i in range(1,6):
-			tmp = str(i)
-			sec = '/html/body/div[1]/section[5]/ol/li[' + tmp + ']/article/div/h2/a/text()'
-			link1 = '/html/body/div[1]/section[5]/ol/li[' + tmp + ']/article/div/h2/a/@href'
-			#print(sec, link1)
-			data = str(tree.xpath(sec))
-			data2 = str(tree.xpath(link1))
-			obj1 = {}
-			obj1["title"] = data[2:len(data)-2]
-			obj1["link"] = url[:len(url)-1]+data2[2:len(data2)-2]
-			li.append(obj1)
-		j = json.dumps(li,  separators=(",\n ", ":"))
-		#print (j)
-		return j
+	url = "https://time.com/"
+	res = requests.get(url = url)
+	tree = html.fromstring(res.content )
+	#print(tree)
+	li = []
+	for i in range(1,6):
+		tmp = str(i)
+		sec = '/html/body/div[1]/section[5]/ol/li[' + tmp + ']/article/div/h2/a/text()'
+		link1 = '/html/body/div[1]/section[5]/ol/li[' + tmp + ']/article/div/h2/a/@href'
+		#print(sec, link1)
+		data = str(tree.xpath(sec))
+		data2 = str(tree.xpath(link1))
+		obj1 = {}
+		obj1["title"] = data[2:len(data)-2]
+		obj1["link"] = url[:len(url)-1]+data2[2:len(data2)-2]
+		li.append(obj1)
+	j = json.dumps(li,  separators=(",\n ", ":"))
+	#print (j)
+	return j
         
 
 if __name__=="__main__":
